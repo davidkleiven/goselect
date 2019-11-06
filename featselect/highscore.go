a6fed9589e29e0b14d3a1ad86a9cf23500a9893d
David Kleiven <davidkleiven446@gmail.com>
2019-11-06 17:26:47 +0100

    ENH: Implement highscore list

---
+++ featselect/highscore.go
@@ -0,0 +1,53 @@
+package featselect
+
+import (
+	"container/list"
+)
+
+// Highscore is a structure that holds a list of Nodes sorted by their score
+type Highscore struct {
+	items    *list.List
+	maxItems int
+}
+
+// NewHighscore creates a new highscore list with maxItems entries
+func NewHighscore(maxItems int) *Highscore {
+	var highscore Highscore
+	highscore.items = list.New()
+	highscore.maxItems = maxItems
+	return &highscore
+}
+
+// Insert insters a new node into the highscore list
+func (highscore *Highscore) Insert(node *Node) {
+	if highscore.Len() == highscore.maxItems-1 {
+		last := highscore.items.Back()
+		if last.Value.(*Node).score > node.score {
+			return
+		}
+	}
+
+	itemInserted := false
+	for item := highscore.items.Front(); item != nil; item = item.Next() {
+		if item.Value == nil {
+			panic("Insert: Value is nil")
+		}
+		if item.Value.(*Node).score < node.score {
+			highscore.items.InsertBefore(node, item)
+			itemInserted = true
+			if highscore.items.Len() >= highscore.maxItems {
+				highscore.items.Remove(highscore.items.Back())
+			}
+			break
+		}
+	}
+
+	if !itemInserted {
+		highscore.items.PushBack(node)
+	}
+}
+
+// Len returns the number of items in the highscore list
+func (h *Highscore) Len() int {
+	return h.items.Len()
+}
---
+++ featselect/highscore_test.go
@@ -0,0 +1,59 @@
+package featselect
+
+import (
+	"testing"
+
+	"gonum.org/v1/gonum/floats"
+)
+
+func TestHighscore(t *testing.T) {
+	highscore := NewHighscore(4)
+
+	var node1 Node
+	node1.score = 1.0
+	highscore.Insert(&node1)
+
+	if highscore.Len() != 1 {
+		t.Errorf("Highscore: Expected length 1. Got %v", highscore.Len())
+	}
+
+	var node2 Node
+	node2.score = 0.5
+	highscore.Insert(&node2)
+
+	expected := []float64{1.0, 0.5}
+	scores := getScores(highscore)
+	if !floats.EqualApprox(scores, expected, 1e-10) {
+		t.Errorf("Highscores: Expected: %v, Got: %v", expected, scores)
+	}
+
+	// Insert a node in between
+	var node3 Node
+	node3.score = 0.75
+	highscore.Insert(&node3)
+	scores = getScores(highscore)
+	expected = []float64{1.0, 0.75, 0.5}
+	if !floats.EqualApprox(scores, expected, 1e-10) {
+		t.Errorf("Highscores: Expected: %v, Got: %v", expected, scores)
+	}
+
+	// Insert new node at end
+	var node4 Node
+	node4.score = 0.6
+	highscore.Insert(&node4)
+	expected = []float64{1.0, 0.75, 0.6}
+	scores = getScores(highscore)
+	if !floats.EqualApprox(scores, expected, 1e-10) {
+		t.Errorf("Highscores: Expected: %v, Got: %v", expected, scores)
+	}
+}
+
+func getScores(h *Highscore) []float64 {
+	scores := make([]float64, h.Len())
+	i := 0
+	for item := h.items.Front(); item != nil; item = item.Next() {
+		scores[i] = item.Value.(*Node).score
+		i++
+	}
+	return scores
+}
