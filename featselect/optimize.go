9c27705f9d468aed7bb8dac60ff7d1a6c567c9a1
David Kleiven <davidkleiven446@gmail.com>
2019-11-06 20:23:36 +0100

    ENH: Implement branch and bound

--- featselect/optimize.go
+++ featselect/optimize.go
@@ -1,5 +1,16 @@
 package featselect
 
+import (
+	"container/list"
+	"math"
+
+	"gonum.org/v1/gonum/mat"
+)
+
+// MaxFeatures limits the maximum number of features allowed in the models considered.
+// It is given as a fraction of the number of available features
+const MaxFeatures = 0.8
+
 // Gcs returns the greatest common model (gcs).
 // GCS is equal to the model with the largest
 // amount of features, given that all "bits"
@@ -35,4 +46,62 @@ func NumFeatures(model []bool) int {
 		}
 	}
 	return num
+}
+
+func SelectModel(X *mat.Dense, y []float64) *Highscore {
+	highscore := NewHighscore(100)
+	queue := list.New()
+
+	nrows, ncols := X.Dims()
+
+	if ncols < 3 {
+		panic("SelectModel: The number of features has to be larger or equal to 3.")
+	}
+
+	emptyModel := make([]bool, ncols)
+	rootNode := NewNode(0, emptyModel)
+	queue.PushBack(rootNode)
+	maxFeatures := int(math.Min(MaxFeatures*float64(nrows), float64(nrows-2)))
+	log2Pruned := 0
+	numChecked := 0.0
+	for queue.Front() != nil {
+		node := queue.Front().Value.(*Node)
+		n := NumFeatures(node.model)
+
+		if n > 0 {
+			design := GetDesignMatrix(node.model, X)
+			node.coeff = Fit(design, y)
+			rss := Rss(design, node.coeff, y)
+			aicc := Aicc(n, nrows, rss)
+			node.score = -aicc
+			highscore.Insert(node)
+			numChecked++
+		}
+		queue.Remove(queue.Front())
+
+		// Create the child nodes
+		leftChild := GetChildNode(node, false)
+		n = NumFeatures(leftChild.model)
+
+		if n > 0 && n < maxFeatures {
+			leftChild.lower, leftChild.upper = BoundsAICC(leftChild.model, leftChild.level, X, y)
+			if leftChild.lower < math.Abs(highscore.BestScore()) {
+				queue.PushBack(leftChild)
+			} else {
+				log2Pruned += ncols - leftChild.level
+			}
+		}
+
+		rightChild := GetChildNode(node, true)
+		n = NumFeatures(rightChild.model)
+		if n > 0 && n < maxFeatures {
+			rightChild.lower, rightChild.upper = BoundsAICC(rightChild.model, rightChild.level, X, y)
+			if rightChild.lower < math.Abs(highscore.BestScore()) {
+				queue.PushBack(rightChild)
+			} else {
+				log2Pruned += ncols - rightChild.level
+			}
+		}
+	}
+	return highscore
 }
