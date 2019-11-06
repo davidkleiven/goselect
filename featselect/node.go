5e45a33690383b6025c20f372d5890c37dfd83ca
David Kleiven <davidkleiven446@gmail.com>
2019-11-06 15:57:24 +0100

    ENH: Add lower, upper and score to the node type

--- featselect/node.go
+++ featselect/node.go
@@ -1,11 +1,19 @@
 package featselect
 
+// Node is a type that holds an array of bools (model) indicating which
+// features are active. The leven field tells which level in the tree this
+// node is on. The lower and upper fields represents bounds of the score
+// for all models that are childrens of this node. The score field holsd
+// the score of this model.
 type Node struct {
 	model []bool
 	level int
+	lower float64
+	upper float64
+	score float64
 }
 
-// Create a child not of node. parent is the parent node the boolean right is true
+// GetChildNode creates a child not of node. parent is the parent node the boolean right is true
 // if the child node should be in the left in the tree. The left child node is
 // characterised with the bit corresponding to the level of the parent is set to
 // false, while in the right child node it is set to true
@@ -18,7 +26,7 @@ func GetChildNode(parent *Node, right bool) Node {
 	return child
 }
 
-// Compare two nodes
+// NodesEqual compare two nodes
 func NodesEqual(node1 *Node, node2 *Node) bool {
 	if len(node1.model) != len(node2.model) {
 		return false
@@ -36,7 +44,7 @@ func NodesEqual(node1 *Node, node2 *Node) bool {
 	return true
 }
 
-// Create a new node
+// NewNode creates a new node
 func NewNode(level int, model []bool) Node {
 	var node Node
 	node.level = level
