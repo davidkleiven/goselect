c013dd8c9820498402589021f17db85ffe0deef3
David Kleiven <davidkleiven446@gmail.com>
2019-11-08 17:49:24 +0100

    STY: Rename rss to logL

--- featselect/criteria.go
+++ featselect/criteria.go
@@ -9,24 +9,24 @@ import (
 // RssTol is the minimum residual sum of squares allowed. It is introduced to avoid problems inside math.Log
 const RssTol = 1e-12
 
-type crit func(num_feat int, num_data int, rss float64) float64
+type crit func(num_feat int, num_data int, logL float64) float64
 
 // Aic calculates the value of AIC given a number of selected
 // featues (num_feat), number of data points (num_data) and
-// the residual sum of squares (rss)
-func Aic(numFeat int, numData int, rss float64) float64 {
-	return 2.0*float64(numFeat) + float64(numData)*math.Log(rss)
+// the log of the likelihood function
+func Aic(numFeat int, numData int, logL float64) float64 {
+	return 2.0*float64(numFeat) + float64(numData)*math.Log(logL)
 }
 
 // Aicc calculates the corrected Aic which is more accurate
 // when the sample size is small
-func Aicc(numFeat int, numData int, rss float64) float64 {
+func Aicc(numFeat int, numData int, logL float64) float64 {
 	denum := float64(numData - numFeat - 1)
 	if numFeat >= numData-1 {
 		denum = 1.0
 	}
 
-	return Aic(numFeat, numData, rss) + float64(2*numFeat*numFeat+2*numFeat)/denum
+	return Aic(numFeat, numData, logL) + float64(2*numFeat*numFeat+2*numFeat)/denum
 }
 
 // Calculata a lower and upper bound of all sub-models. The bits up until
