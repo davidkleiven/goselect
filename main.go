65d509bcca1f6087301c6964195ee0e1cd4feb9d
David Kleiven <davidkleiven446@gmail.com>
2019-11-12 09:20:10 +0100

    BUG: Code did not terminate on finish

--- main.go
+++ main.go
@@ -19,6 +19,10 @@ func saveHighscoreList(fname string, h *featselect.Highscore) {
 	ioutil.WriteFile(fname, highscoreJSON, 0644)
 }
 
+func setSearchFinished(finished chan int) {
+	finished <- 0
+}
+
 func main() {
 	args := featselect.ParseCommandLineArgs(os.Args[1:])
 	dset := featselect.ReadCSV(args.Csvfile, args.TargetCol)
@@ -27,23 +31,30 @@ func main() {
 
 	var wg sync.WaitGroup
 	var progress featselect.SearchProgress
+	searchFinished := make(chan int)
 	highscore := featselect.NewHighscore(1000)
 
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
+		defer setSearchFinished(searchFinished)
 		featselect.SelectModel(dset.X, dset.Y, highscore, &progress)
 	}()
 
 	c := time.Tick(60 * time.Second)
 	fmt.Printf("Saving highscore list periodically to " + args.Outfile + "\n")
+
+timeloop:
 	for {
 		select {
 		case <-c:
 			score, numChecked, log2Pruned := progress.Get()
 			fmt.Printf("%v: Score: %f, Num. checked: %d, Log2 pruned: %d\n", time.Now(), score, numChecked, log2Pruned)
 			saveHighscoreList(args.Outfile, highscore)
+		case <-searchFinished:
+			break timeloop
 		}
 	}
+	fmt.Println("Selection finished")
 	wg.Wait()
 }
