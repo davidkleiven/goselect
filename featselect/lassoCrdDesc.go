1360709e195dfb0c880eb8b6f1bad02fb7b41def
David Kleiven <davidkleiven446@gmail.com>
2019-12-12 09:15:04 +0100

    Add coordinate descent lasso

--- featselect/lassoCrdDesc.go
+++ featselect/lassoCrdDesc.go
@@ -1,6 +1,7 @@
 package featselect
 
 import (
+	"fmt"
 	"math"
 
 	"gonum.org/v1/gonum/mat"
@@ -8,11 +9,10 @@ import (
 
 // LassoCrdDesc solves the lasso problem via coordinate descent
 func LassoCrdDesc(dset *NormalizedData, lamb float64, cov CovMat, x0 []float64, maxIter int) []float64 {
-	if x0 == nil {
-		x0 = make([]float64, len(dset.y))
-	}
-
 	nr, nFeat := dset.X.Dims()
+	if x0 == nil {
+		x0 = make([]float64, nFeat)
+	}
 
 	covMat := cov.Get(dset.X)
 
@@ -28,7 +28,7 @@ func LassoCrdDesc(dset *NormalizedData, lamb float64, cov CovMat, x0 []float64, 
 
 	betaOld := x0
 	beta := make([]float64, nFeat)
-	covDotBeta := make([]float64, nFeat)
+	covDotBeta := MulSlice(covMat, betaOld)
 	tol := 1e-10
 	converged := false
 	for iter := 0; iter < maxIter; iter++ {
@@ -56,6 +56,10 @@ func LassoCrdDesc(dset *NormalizedData, lamb float64, cov CovMat, x0 []float64, 
 			break
 		}
 	}
+
+	if !converged {
+		fmt.Printf("Warning! Lasso coordinate descent did not converge within the given number of iterations\n")
+	}
 	return beta
 }
 
@@ -69,10 +73,51 @@ func SoftThreshold(x float64, threshold float64) float64 {
 	return 0.0
 }
 
+// MulSlice multiplies together a matrix and a vector
+func MulSlice(X mat.Matrix, v []float64) []float64 {
+	nr, nc := X.Dims()
+	if len(v) != nc {
+		panic("Inconsistent length of passed vector")
+	}
+
+	res := make([]float64, nr)
+	for i := 0; i < nr; i++ {
+		for j := 0; j < nc; j++ {
+			res[i] += X.At(i, j) * v[j]
+		}
+	}
+	return res
+}
+
 // UpdateCovDotBeta updates dot product between a matrix and an vector when one item changes
 func UpdateCovDotBeta(cov mat.Matrix, covDotBeta []float64, coeffNo int, oldCoeff float64, newCoeff float64) []float64 {
 	for i := 0; i < len(covDotBeta); i++ {
 		covDotBeta[i] += cov.At(i, coeffNo) * (newCoeff - oldCoeff)
 	}
 	return covDotBeta
+}
+
+// LassoCrdDescPath calculates a set of lasso solutions along equi-logspaced set of lambda values
+func LassoCrdDescPath(dset *NormalizedData, cov CovMat, lambs []float64, maxIter int) []*LassoLarsNode {
+	_, nFeat := dset.X.Dims()
+	x0 := make([]float64, nFeat)
+
+	nodes := make([]*LassoLarsNode, len(lambs))
+	for i := 0; i < len(lambs); i++ {
+		lamb := lambs[len(lambs)-i-1]
+		coeff := LassoCrdDesc(dset, lamb, cov, x0, maxIter)
+		copy(x0, coeff)
+
+		selection := []int{}
+		selectedCoeff := []float64{}
+		for j := range coeff {
+			if math.Abs(coeff[j]) > lamb/2. {
+				selection = append(selection, j)
+				selectedCoeff = append(selectedCoeff, coeff[j])
+			}
+		}
+
+		nodes[i] = NewLassoLarsNode(selectedCoeff, lamb, selection)
+	}
+	return nodes
 }
--- featselect/lassoCrdDesc_test.go
+++ featselect/lassoCrdDesc_test.go
@@ -1,7 +1,6 @@
 package featselect
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/davidkleiven/goselect/featselect/testfeatselect"
@@ -13,7 +12,6 @@ func TestLassoCrdDesc(t *testing.T) {
 	data := NewNormalizedData(X, y)
 	var cov Empirical
 	res := LassoCrdDesc(data, 0.00001, &cov, nil, 100000)
-	fmt.Printf("%v\n", res)
 	res[0] = data.LinearTransformationBias([]int{1, 2, 3, 4}, res[1:])
 	for i := 1; i < 5; i++ {
 		res[i] = data.LinearNormalizationTransformation(i, res[i])
@@ -23,5 +21,29 @@ func TestLassoCrdDesc(t *testing.T) {
 
 	if !floats.EqualApprox(res, expect, 1e-4) {
 		t.Errorf("LassoCrdDesc: expected\n%v\nGot\n%v\n", expect, res)
+	}
+}
+
+func TestLassoCrdDescPath(t *testing.T) {
+	X, y := testfeatselect.GetExampleXY()
+	data := NewNormalizedData(X, y)
+
+	lambs := Logspace(1e-8, 1.0, 20)
+	var cov Empirical
+	res := LassoCrdDescPath(data, &cov, lambs, 100000)
+	Path2Unnormalized(data, res)
+
+	last := res[len(res)-1]
+	expectSel := []int{0, 1, 2}
+	for i := range expectSel {
+		if expectSel[i] != last.Selection[i] {
+			t.Errorf("unexpected selection. Expected\n%v\nGot\n%v\n", expectSel, last.Selection)
+			break
+		}
+	}
+
+	expectCoeff := []float64{-3., 3., 0.2}
+	if !floats.EqualApprox(expectCoeff, last.Coeff, 1e-4) {
+		t.Errorf("unexpected coefficients. Expected\n%v\nGot\n%v\n", expectCoeff, last.Coeff)
 	}
 }
